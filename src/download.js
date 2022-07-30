a7a6fbe530807bf7c50ee7d2390ac447ff50b970
NOVA <jvcooray@gmail.com>
2022-07-31 01:29:04 +0530

    removed lb module

--- src/download.js
+++ src/download.js
@@ -13,7 +13,7 @@ module.exports.file_proc = function (files,end){
             let real_path = file_path.replace("3cs-mb-key-","http://");
         let url = new URL(real_path);
         let options = {
-            host: "127.0.0.1",
+            host: "10.122.0.3",
             port: 80,
             path: `http://${url.hostname}${url.pathname}${url.search}`,
             headers: {
